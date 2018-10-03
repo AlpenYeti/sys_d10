2528d701ee3f7106767dbbe7ab079db56570bbab
Dandaedre <max.rigaut@gmail.com>
2018-10-03 17:14:43 +0200

    Intermediate commit, still upgrading the generator

--- skill_generator.py
+++ skill_generator.py
@@ -26,46 +26,59 @@ except:
 "replace":-1,"add_to_all":0,"max_dices":-1,"player_name":""};"""
 
 list=[]
-#list.append(['varname','Pretty name','nb_args','type','code','id','default'])
-list.append(['perfection','Perfection',1,'int','P',0,0])
-list.append(['defense_i_0','Defence impénétrable simple',2,'int','I',1,0])
-list.append(['defense_i_1','Defence impénétrable expert',2,'int','I',2,0])
-list.append(['defense_i_2','Defence impénétrable meta',2,'int','I',4,0])
-list.append(['rempart_p','Rempart parfait',1,'int','R',0,0])
-list.append(['technique_m','Perfection',1,'int','M',0,0])
-list.append(['coup_d','Coup_déchirant',1,'int','D',0,0])
-list.append(['fauchage','Fauchage',1,'int','F',0,0])
-list.append(['exploiter_p_0','Exploiter_les_points_faibles simple',2,'int','E',1,0])
-list.append(['exploiter_p_1','Exploiter_les_points_faibles expert',2,'int','E',2,0])
-list.append(['exploiter_p_2','Exploiter_les_points_faibles meta',2,'int','E',4,0])
-list.append(['tir_p_0','Tir_précis expert',2,'int','T',2,0])
-list.append(['tir_p_1','Tir_précis meta',2,'int','T',4,0])
-list.append(['tir_i','Tir_implacable',1,'int','i',0,0])
-list.append(['charge','Charge',1,'int','C',0,0])
-list.append(['charge_i','Charge_indomptable',1,'int','N',0,0])
-list.append(['nb_2add','Nombre à ajouter',1,'int','+',0,0])
-list.append(['nb_2sub','Nombre a soustraire',1,'int','-',0,0])
-list.append(['relances','Relances',1,'int','r',0,0])
-list.append(['seuil','Seuil',1,'int','s',0,0])
-list.append(['action','Type d\'Action',1,'str','a',0,'""'])
-list.append(['flat_dices','Des fixes',1,'int','d',0,"[]",
-"@flat_dices[@flat_dices]=type({tab+1});\n@nb_flat_dices++;"])
-list.append(['on_hit_c','Dégats a l\'impact critique',1,'int','H',0,0])
-list.append(['attribute','Caractéristique',1,'int','A',0,0])
-list.append(['encaissement','Encaissement',2,'int','S',0,0,])
-list.append(['replace','Des de substitution',1,'int','L',0,-1])
-list.append(['add_to_all','Valeur d\'ajout aux des',1,'int','l',0,0])
-list.append(['max_dices','Nombre de des max',1,'int','m',0,-1])
-list.append(['player_name','Nom du joueur',1,'int',':',0,'""'])
+#list.append(['varname','Pretty name','nb_args','type','code','id','default','switch','return'])
+list.append(['perfection','Perfection',1,'int','P',0,0,None,None])
+list.append(['defense_i_0','Defence impénétrable simple',2,'int','I',1,0,None,None])
+list.append(['defense_i_1','Defence impénétrable expert',2,'int','I',2,0,None,None])
+list.append(['defense_i_2','Defence impénétrable meta',2,'int','I',4,0,None,None])
+list.append(['rempart_p','Rempart parfait',1,'int','R',0,0,None,None])
+list.append(['technique_m','Perfection',1,'int','M',0,0,None,None])
+list.append(['coup_d','Coup_déchirant',1,'int','D',0,0,None,None])
+list.append(['fauchage','Fauchage',1,'int','F',0,0,None,None])
+list.append(['exploiter_p_0','Exploiter_les_points_faibles simple',2,'int','E',1,0,None,None])
+list.append(['exploiter_p_1','Exploiter_les_points_faibles expert',2,'int','E',2,0,None,None])
+list.append(['exploiter_p_2','Exploiter_les_points_faibles meta',2,'int','E',4,0,None,None])
+list.append(['tir_p_0','Tir_précis expert',2,'int','T',2,0,None,None])
+list.append(['tir_p_1','Tir_précis meta',2,'int','T',4,0,None,None])
+list.append(['tir_i','Tir_implacable',1,'int','i',0,0,None,None])
+list.append(['charge','Charge',1,'int','C',0,0,None,None])
+list.append(['charge_i','Charge_indomptable',1,'int','N',0,0,None,None])
+list.append(['nb_2add','Nombre à ajouter',1,'int','+',0,0,None,None])
+list.append(['nb_2sub','Nombre a soustraire',1,'int','-',0,0,None,None])
+list.append(['relances','Relances',1,'int','r',0,0,None,None])
+list.append(['seuil','Seuil',1,'int','s',0,0,None,None])
+list.append(['action','Type d\'Action',1,'str','a',0,'""',None,None])
+list.append(['flat_dices','Des fixes',1,'int','d',0,"[]",lambda_flat_dices,None])
+list.append(['on_hit_c','Dégats a l\'impact critique',1,'int','H',0,0,None,None])
+list.append(['attribute','Caractéristique',1,'int','A',0,0,None,None])
+list.append(['encaissement','Encaissement',2,'int','S',0,0,,None,None])
+list.append(['replace','Des de substitution',1,'int','L',0,-1,None,None])
+list.append(['add_to_all','Valeur d\'ajout aux des',1,'int','l',0,0,None,None])
+list.append(['max_dices','Nombre de des max',1,'int','m',0,-1,None,None])
+list.append(['player_name','Nom du joueur',1,'int',':',0,'""',None,None])
 
-list.append(['nb_dices','Nombre de des',1,'int','',0,0])
-list.append(['nb_flat_dices','Nombre de des fixes',1,'int','',0,0])
-list.append(['encaissement_dices','Perfection',1,'int','',0,'""'])
-list.append(['results','Resultats',1,'int','',0,"[]"])
-list.append(['cleave','Fauchage',1,'int','',0,"[]"])
-list.append(['encaissement_result','Resultat d\'encaissement',1,'int','',0,0])
-list.append(['technique_result','Dégats technique martiale',1,'int','',0,0])
-list.append(['coup_d_results','Coup déchirant results',1,'int','',0,"[]"])
+list.append(['nb_dices','Nombre de des',1,'int','',0,0,None,None])
+list.append(['nb_flat_dices','Nombre de des fixes',1,'int','',0,0,None,None])
+list.append(['encaissement_dices','Perfection',1,'int','',0,'""',None,None])
+list.append(['results','Resultats',1,'int','',0,"[]",None,None])
+list.append(['cleave','Fauchage',1,'int','',0,"[]",None,None])
+list.append(['encaissement_result','Resultat d\'encaissement',1,'int','',0,0,None,None])
+list.append(['technique_result','Dégats technique martiale',1,'int','',0,0,None,None])
+list.append(['coup_d_results','Coup déchirant results',1,'int','',0,"[]",None,None])
+
+class whateverWrapper():
+    def __init__(self,table):
+        self.table=table
+        self.indexes={}
+        for i in range(table):
+            self.indexes[table[i]]=table[i][0]
+
+    def __get__(self,argument): #Actually return the full line
+        return self.table[self.indexes[argument]]
+## Custom adders:
+lambda_flat_dices=lambda_flat_dices a: "@flat_dices[@flat_dices]=type({tab+1});\n@nb_flat_dices++;"
+
+## Custom rerolls:
 
 ## Commands
 def initialise(args):
@@ -128,10 +141,11 @@ def check_init(args):
         print("Init: All test sucessfull")
     return fails
 
-def returns(args):
-    pass
+def reroll(args):
+    reroll="var msg_relance=\"<a class='sheet-rolltemplate-d10fight' href='!crit 0 "
 
-def check_returns(args):
+
+def check_reroll(args):
     pass
 
 
